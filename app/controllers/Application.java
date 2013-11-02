f4280a9b3ac6c9b8dc87153e422e162d5233737e
jamestvu <jamesvu@hawaii.edu>
2013-11-01 22:01:59 -1000

    Added header image.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.formdata.StudentFormData;
 import views.html.Index;
 import views.html.Page1;
 
@@ -26,4 +28,16 @@ public class Application extends Controller {
     return ok(Page1.render("Welcome to Page1."));
     
   }
+  
+  /**
+   * Returns the manage student page.
+   * @return The manage student page.
+   */
+  public static Result newStudent() {
+    StudentFormData data = new StudentFormData();
+    Form<StudentFormData> formData = Form.form(StudentFormData.class).fill(data);
+    return ok(Page1.render("Welcome to Page1."));
+
+  }
+  
 }
