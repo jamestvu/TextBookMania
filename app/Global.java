911b94811aa58d68bb1617c296613269ccdaaca5
jamestvu <jamesvu@hawaii.edu>
2013-11-01 21:29:16 -1000

    added checkstyle

---
+++ app/Global.java
@@ -0,0 +1,22 @@
+
+import play.Application;
+import play.GlobalSettings;
+
+
+
+/**
+ * Overrides the startup.
+ * @author Owner
+ *
+ */
+public class Global extends GlobalSettings {
+  
+  /**
+   * Initializes the surfers upon startup.
+   * @param app The application.
+   */
+  public void onStart(Application app) {
+    
+  }
+
+}
